2f23b6e7029c1b9c86f3844357da6412567e3047
adampauw <adampauw@gmail.com>
2018-02-05 21:03:59 -0800

    submitting to hub

---
+++ assignment-1.js
@@ -0,0 +1,15 @@
+var numbers = [];
+
+for (var i=0; i < 10; i++){
+    numbers.push(i);
+}
+
+console.log(numbers[0]);
+console.log(numbers[9]);
+/*console.log(numbers);*/
+
+var car = {};
+
+car.colour = "blank"
+  
+console.log(car.colour);
